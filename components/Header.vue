d9b32148009a2ad0dae826e020cd8eff2dff46f3
zal3 <zainabamjedlatef31@gmail.com>
2022-11-15 23:00:53 +0300

    vuestic

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,10 @@
 <template>
     <nav flex="~ " justify="between" px="20" h="20"  left="0" items="center"  top="0" bg="#D9D9D9" >
             <NuxtLink to="/" un-text="3xl  black" font="bold">Events</NuxtLink>
-        <div>
+        <div flex="~" items="center">
             <!-- <div class="invisible group-hover:visible i-ic-baseline-plus"></div> -->
-            <div text="black" class="i-ic-baseline-plus"></div>
+            <!-- <div text="black" class="i-ic-baseline-plus"></div> -->
+            <va-icon name="fas fa-plus" class=" h-full" />
             <span>Add Event</span>
         </div>
     </nav>
--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,5 @@
         <Event />
     </div>
 </template>
+<script setup>
+</script>
