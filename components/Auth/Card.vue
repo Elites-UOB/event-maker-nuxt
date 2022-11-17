364ebb70529b09a0e75a84fbe012271009787956
zal3 <zainabamjedlatef31@gmail.com>
2022-11-17 18:01:36 +0300

    sign

--- components/Auth/Card.vue
+++ components/Auth/Card.vue
@@ -1,19 +1,31 @@
 <template>
     <div grid="~ cols-1 gap-4" w="1/2" P="4">
-        <h1>{{authState}} </h1>
-            <va-input placeholder="email" bordered type="email" />
-            <va-input placeholder="password" bordered type="password" />
-         <va-button preset="primary" class="mr-4 mb-2" @click="handleSubmit">Submit</va-button>
-        <p text="blue-800" @click="toggleAuthState">{{ authState === 'Login' ? 'Create new account' : 'Already have an account' }}</p>
+        <h1>{{ authState }}</h1>
+        <va-input placeholder="email" bordered type="email" v-model="input.email" />
+        <va-input placeholder="password" bordered type="password" v-model="input.password" />
+        <va-button preset="primary" class="mr-4 mb-2" @click="handleSubmit">Submit</va-button>
+        <va-button preset="primary" class="mr-4 mb-2" @click="signOut">Logout</va-button>
+        <p text="blue-800" @click="toggleAuthState">{{ authState === 'Login' ? 'Create new account' : 'Already have anaccount' }}</p>
+        {{user}}
     </div>
 </template>
 <script setup lang="ts">
-const { signUp , user } = useAuth();
+const { signUp, signIn, signOut, user } = useAuth();
+const input = reactive({
+    email: '',
+    password: '',
+});
 const authState = ref<'Login' | 'Singup'>('Login')
 const toggleAuthState = () => {
     authState.value = authState.value === 'Login' ? 'Singup' : 'Login'
 }
 const handleSubmit = () => {
-    signUp({ email: 'zainab@gmail.com', password: 'password' })
+    if (authState.value === 'Login') {
+        signIn({ email: input.email, password: input.password })
+    } else {
+        signUp({ email: input.email, password: input.password })
+    }
+    input.email = ''
+    input.password = ''
 }
 </script>
--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -20,9 +20,26 @@ const useAuth = () => {
         if (error) throw error
         return u
     }
+
+    const signIn = async ({ email, password }) => {
+        const { user: u, error } = await supabase.auth.signIn({
+                email,
+                password,
+            })
+        if (error) throw error
+        return u
+    }
+
+    const signOut = async () => {
+        const { error } = await supabase.auth.signOut()
+        if (error) throw error  
+    }
+
     return {
         user,
         signUp,
+        signIn,
+        signOut,
     }
 }
 export default useAuth 
