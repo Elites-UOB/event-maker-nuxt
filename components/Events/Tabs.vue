309ce73a2a00e6d58f824d69b8a1908aad09e29e
zal3 <zainabamjedlatef31@gmail.com>
2022-11-22 07:44:32 +0300

    a

--- components/Events/Tabs.vue
+++ components/Events/Tabs.vue
@@ -15,15 +15,9 @@
                 index == 0 ? 'rounded-t-xl ' : '',
                 index == 2 ? 'rounded-b-xl ' : '',
             ]" p="3">
-                <svg v-if="index == 0" w="10"  height="28" viewBox="0 0 22 28" fill="none" flex="~" justify="center" items="center">
-                    <Icon name="mdi-file-document" text="white" size="20" />
-                </svg>
-                <svg v-if="index == 1" w="7"  height="28" viewBox="0 0 32 32" fill="none" flex="~" justify="center" items="center">
-                    <Icon name="ic-baseline-people-alt" text="white" size="20" />
-                </svg>
-                <svg v-if="index == 2" w="7"  height="28" viewBox="0 0 30 24" fill="none" flex="~" justify="center" items="center">
-                    <Icon name="mdi-barcode-scan" text="white" size="20"  />
-                </svg>
+                    <Icon name="mdi-file-document" text="white" size="20" fill="none" v-if="index == 0" flex="~" justify="center" items="center" />
+                    <Icon name="ic-baseline-people-alt" v-if="index == 1" text="white" size="20" fill="none" flex="~" justify="center" items="center" />
+                    <Icon name="mdi-barcode-scan" v-if="index == 2" text="white" size="20" flex="~" justify="center" items="center"  />
             </div>
         </div>
 
@@ -35,15 +29,11 @@
                 index == 0 ? 'rounded-r-xl ' : '',
                 index == 2 ? 'rounded-l-xl ' : '',
             ]" p="3">
-                <svg v-if="index == 0" w="10"  height="28" viewBox="0 0 22 28" fill="none" flex="~" justify="center" items="center">
-                    <Icon name="mdi-file-document" text="white" size="20" />
-                </svg>
-                <svg v-if="index == 1" w="7"  height="28" viewBox="0 0 32 32" fill="none" flex="~" justify="center" items="center">
-                    <Icon name="ic-baseline-people-alt" text="white" size="20" />
-                </svg>
-                <svg v-if="index == 2" w="7"  height="28" viewBox="0 0 30 24" fill="none" flex="~" justify="center" items="center">
-                    <Icon name="mdi-barcode-scan" text="white" size="20"  />
-                </svg>
+                <Icon name="mdi-file-document" text="white" size="20" fill="none" v-if="index == 0" flex="~" justify="center"
+                    items="center" />
+                <Icon name="ic-baseline-people-alt" v-if="index == 1" text="white" size="20" fill="none" flex="~" justify="center"
+                    items="center" />
+                <Icon name="mdi-barcode-scan" v-if="index == 2" text="white" size="20" flex="~" justify="center" items="center" />
             </div>
         </div>
 
