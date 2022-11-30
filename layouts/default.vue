0ec41d08c1c43a21f36cc8cdb22b192909af51e8
william-iq397 <93004634+william-iq397@users.noreply.github.com>
2022-11-30 20:31:09 +0300

    done

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,11 +2,7 @@
     <div flex="~ col " h="full" font="sans">
         <Header />
         <div flex="grow">
-            <Transition enter-active-class="  transform  duration-800 ease-out" enter-from-class=" opacity-0"
-                enter-to-class=" opacity-100" leave-active-class=" transform  " leave-from-class="opacity-100"
-                leave-to-class="opacity-0">
-                <slot />
-            </Transition>
+            <slot />
         </div>
     </div>
 </template>
